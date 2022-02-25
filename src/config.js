f7c761c50512811b8ca90608cc0f388579bd003b
Greg Santos <greg.santos@dapperlabs.com>
2022-02-25 13:58:00 -0800

    Update fcl and add http

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 import * as fcl from "@onflow/fcl"
+import {send as httpSend} from "@onflow/transport-http"
+import {send as grpcSend} from "@onflow/transport-grpc"
 
 const USE_LOCAL = false
 
@@ -10,14 +12,26 @@ fcl.config()
 
 if (USE_LOCAL) {
   // prettier-ignore
-  fcl.config()
+  fcl
+    .config()
     .put("env", "local")
     .put("accessNode.api", "http://localhost:8080")
+    .put("sdk.transport", grpcSend)
     .put("discovery.wallet", "http://localhost:8701/fcl/authn")
 } else {
   // prettier-ignore
-  fcl.config()
+  fcl
+    .config()
     .put("env", "testnet")
-    .put("accessNode.api", "https://access-testnet.onflow.org")
+    // http
+    .put("accessNode.api", "https://rest-testnet.onflow.org")
+    .put("sdk.transport", httpSend)
+    .put("discovery.authn.include", ["0x9d2e44203cb13051"])
+    // grpc
+    // .put("accessNode.api", "https://access-testnet.onflow.org")
+    // .put("sdk.transport", grpcSend)
     .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn")
+  // mainnet
+  //.put("accessNode.api", "https://access-mainnet-beta.onflow.org")
+  //.put("discovery.wallet", "https://fcl-discovery.onflow.org/authn")
 }
